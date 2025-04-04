07be75227ea5795318f41606bf960f136820b2d3
Maverick9081 <faliyaabhishek@gmail.com>
2025-04-04 21:30:23 +0530

    Cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,21 @@
-use log::info;
+use log::{debug, error, info, warn};
 use logger::init_logger;
 use prost::Message;
 use solana_sdk::{
     pubkey::Pubkey,
     transaction::{SanitizedVersionedTransaction, VersionedTransaction},
 };
-use std::{env, net::Ipv4Addr, thread};
+use std::{env, fs, path::Path, thread};
 use std::{str::FromStr, thread::sleep, time::Duration};
-use tokio::{net::UdpSocket, runtime::Runtime};
 use types::{Opcode, Request};
-use zmq::DONTWAIT;
 pub mod logger;
 
 mod types {
     include!(concat!(env!("OUT_DIR"), "/_.rs"));
 }
-// const PULL_UDS_PATH: &str = "/tmp/dock.sock";
-// const PUSH_UDS_PATH: &str = "/tmp/push.sock";
+
 const TCP_PUSH_ADDR: &str = "tcp://65.108.233.175:8080";
 const TCP_PULL_ADDR: &str = "tcp://65.108.233.175:8081";
-
-// const LOCAL_INTERFACE: &str = "0.0.0.0";
-// // const LOCAL_INTERFACE: &str = "65.108.233.175";
-// const UDP_PORT: u16 = 8081;
-// const MULTICAST_ADDR: &str = "239.1.1.1";
-
 const XAND_SHILED_KEY: &str = "xSHLJPXU8QW3A9kGiRoL94bksJ7ZZPY4dUwJPAT8CVK";
 
 fn main() {
@@ -61,13 +52,21 @@ fn main() {
         _ => unreachable!(),
     };
 
-    log::info!("UDS Pull Socket path : {}", pull_uds_path);
-    log::info!("UDS Push Socket path : {}", push_uds_path);
-    log::info!("Tcp Pull ip  : {}", TCP_PULL_ADDR);
-    log::info!("Tcp Push ip : {}", TCP_PUSH_ADDR);
+    info!("UDS Pull Socket path : {}", pull_uds_path);
+    info!("UDS Push Socket path : {}", push_uds_path);
+    info!("Tcp Pull ip  : {}", TCP_PULL_ADDR);
+    info!("Tcp Push ip : {}", TCP_PUSH_ADDR);
+
+    let dir_path = Path::new("/var/run/xandeum");
+    // Creating The xandeum directory for Uds sockets
+    if !dir_path.exists() {
+        // Create the directory and any missing parent directories
+        fs::create_dir_all(dir_path).expect("Failed to create /var/run/xandeum directory");
+    }
 
     let context = zmq::Context::new();
 
+    // Creating zmq sockets for the Back channel
     let uds_push_socket = context.socket(zmq::PUSH).unwrap();
     uds_push_socket
         .connect(&format!("ipc://{}", push_uds_path))
@@ -80,26 +79,27 @@ fn main() {
 
     tcp_pull_socket.send(b"\x01" as &[u8], 0).unwrap();
 
+    // Starting atlas listener thread and Xandeum Agave Forwarder Thread
     thread::spawn(move || {
-        info!("Starting the UDP Multicast Listener");
-
+        info!("Starting Atlas Listener");
         loop {
             match tcp_pull_socket.recv_bytes(0) {
                 Ok(msg) => {
-                    info!("Received bytes from Atlas : {:?}", msg);
+                    debug!("Received Data from Atlas : {:?}", msg);
 
                     match uds_push_socket.send(msg, 0) {
-                        Ok(()) => info!("Forwarded data from UDP to UDS PUSH socket"),
-                        Err(e) => log::error!("Failed to forward to UDS PUSH socket: {:?}", e),
+                        Ok(()) => debug!("Forwarded data from Atlas to UDS PUSH socket"),
+                        Err(e) => error!("Failed to forward to UDS PUSH socket: {:?}", e),
                     }
                 }
                 Err(e) => {
-                    log::error!("Error receiving from UDP socket: {:?}", e);
+                    error!("Error receiving from Atlas: {:?}", e);
                 }
             }
         }
     });
 
+    // Creating sockets To receive from Xandeum Agave And to send to Atlas
     let uds_pull_socket = context.socket(zmq::PULL).unwrap();
     uds_pull_socket
         .connect(&format!("ipc://{}", pull_uds_path))
@@ -110,15 +110,17 @@ fn main() {
         .connect(TCP_PUSH_ADDR)
         .expect("Failed to bind PUB socket");
 
+    // Listening to Xandeum agave for incoming Xtransactions and forwarding
+    // them to Atlas
     loop {
         match uds_pull_socket.recv_bytes(0) {
             Ok(msg) => {
                 let tx: VersionedTransaction = bincode::deserialize(&msg).unwrap();
-                log::info!("received tx from Rpc : {:?}", msg);
+                debug!("Received XTransaction from Rpc : {:?}", msg);
                 let reqs = process_tx_to_proto_structure(tx);
 
                 if reqs.is_empty() {
-                    log::warn!("No request Found, Skipping");
+                    warn!("No request Found, Skipping");
                 }
 
                 for req in reqs {
@@ -129,20 +131,20 @@ fn main() {
 
                     match res {
                         Ok(()) => {
-                            log::debug!("Sent a request : {:?}", req);
+                            debug!("Sent a request : {:?}", req);
                         }
                         Err(e) => {
-                            log::error!("Error Sending Req , Error : {}", e);
+                            error!("Error Sending Req , Error : {}", e);
                         }
                     }
                 }
             }
             Err(zmq::Error::EAGAIN) => {
-                log::info!("No Message Received");
-                sleep(Duration::from_millis(500));
+                info!("No Message Received");
+                sleep(Duration::from_millis(100));
             }
             Err(e) => {
-                log::error!(
+                error!(
                     "Error occurred While Receiving Packets through UDS in zmq, Error : {:?}",
                     e
                 );
@@ -151,19 +153,20 @@ fn main() {
     }
 }
 
+/// To Validate and process the XTransaction to a Request format
 fn process_tx_to_proto_structure(tx: VersionedTransaction) -> Vec<Request> {
     let mut reqs: Vec<Request> = Vec::new();
     let sanitized_tx = SanitizedVersionedTransaction::try_new(tx.clone()).unwrap();
 
-    log::info!("Sanitized Transaction : {:?}", sanitized_tx);
+    debug!("Sanitized Transaction : {:?}", sanitized_tx);
     let msg = sanitized_tx.get_message();
 
     let tx_hash = tx
         .signatures
         .get(0)
-        .map(|sig| sig.to_string()) // Convert to base58 string
+        .map(|sig| sig.to_string())
         .unwrap_or_else(|| {
-            log::warn!("Transaction has no signatures, using default");
+            warn!("Transaction has no signatures, using default");
             "no-signature".to_string()
         });
 
@@ -187,24 +190,22 @@ fn process_tx_to_proto_structure(tx: VersionedTransaction) -> Vec<Request> {
                 .collect();
 
             if signers.is_empty() {
-                log::warn!(
+                warn!(
                     "Instruction {} has no signers, skipping request generation",
                     i
                 );
                 continue;
             }
-
             match ix.data.split_first() {
                 Some((op, data)) => {
                     let opcode = match op {
                         0 => Opcode::Bigbang,
                         1 => Opcode::Armageddon,
                         _ => {
-                            log::warn!("Other Instructions are not supported yet, Skipping");
+                            warn!("Other Instructions are not supported yet, Skipping");
                             continue;
                         }
                     };
-
                     let req = Request {
                         op: opcode as i32,
                         pubkey: signers[0].to_bytes().to_vec(),
@@ -214,7 +215,7 @@ fn process_tx_to_proto_structure(tx: VersionedTransaction) -> Vec<Request> {
                     reqs.push(req);
                 }
                 None => {
-                    log::warn!(
+                    warn!(
                         "Instruction {} has empty data, skipping request generation",
                         i
                     );
