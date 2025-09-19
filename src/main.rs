f602f7b1702160d3c5ab32db267bd790003389d1
Maverick9081 <faliyaabhishek@gmail.com>
2025-09-19 16:59:28 +0530

    Added co-owner field

--- src/main.rs
+++ src/main.rs
@@ -386,6 +386,7 @@ fn process_tx_to_proto_structure(tx: VersionedTransaction) -> Result<Vec<Request
                         8 => Opcode::Rename,
                         9 => Opcode::Copy,
                         13 => Opcode::Move,
+                        14 => Opcode::AssignCoowner,
                         _ => {
                             warn!("Other Instructions are not supported yet, Skipping");
                             continue;
